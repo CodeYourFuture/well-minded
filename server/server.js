75005ccd413da9862f6d01025ed5d8fb40614d85
Ahmad Ali <aallii300300@gmail.com>
2020-08-10 11:11:23 +0100

    add first route , port is dynamic

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({
+        authors: ["Ferhat", "Minko", "Roxana", "Ahmad", "Irving"]
+    })
+})
 
-app.listen(5000, () => {
+
+const Port = process.env.PORT || 5000;
+
+
+app.listen(Port, () => {
 
     console.log("app working ....")
 })
