f5e1b8f50ad8013f9bc5994672544036e15ee55b
Ahmad Ali <aallii300300@gmail.com>
2020-08-10 11:13:54 +0100

    enabling cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
@@ -14,5 +16,9 @@ const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => {
 
-    console.log("app working ....")
+    process.env.PORT ?
+        console.log("app working on  << " + process.env.ROOT_URL + "  >> listening on port  " + Port) //deployed
+        :
+        console.log("app working on  http://localhost:" + Port);  //locally
+
 })
