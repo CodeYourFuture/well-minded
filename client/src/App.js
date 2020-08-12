04d8b34551836c9f13e165e8d702e92f94a264f8
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-08-12 09:51:23 +0100

    create components , add bootstrap link in header

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import domain from './config';
+import Header from './components/Header'
 
 function App() {
 
-  const [authors, setAuthors] = useState([]);
+  // const [authors, setAuthors] = useState([]);
 
-  useEffect(() => {
-    fetch(`${domain}/authors`)
-      .then(res => res.json())
-      .then(result => setAuthors(result.authors))
-  }, [])
+  // useEffect(() => {
+  //   fetch(`${domain}/authors`)
+  //     .then(res => res.json())
+  //     .then(result => setAuthors(result.authors))
+  // }, [])
+  // return (
+  //   <div className="App-header">
+  //     <h1>Authors:</h1>
+  //     <ul>
+  //       {
+  //         authors.length > 0 ? authors.map((a, i) => <li key={i}>{a}</li>) : <p>loading...</p>
+  //       }
+  //     </ul>
+  //   </div>
+  // );
+
   return (
     <div className="App-header">
       <h1>Authors:</h1>
-      <ul>
-        {
-          authors.length > 0 ? authors.map((a, i) => <li key={i}>{a}</li>) : <p>loading...</p>
-        }
-      </ul>
+      <Header/>
     </div>
-  );
+    
+  )
 }
 
 export default App;
