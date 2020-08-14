a030b95710295c78c7f76fb9397d25149cd244e1
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-08-14 14:18:07 +0100

    create components, header, nav link , home page

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "../css/Carousel.css";
+
+
+import imageOne from "../images/image 1.jpg";
+import imageTwo from "../images/image 2.jpg";
+import imageThree from "../images/image 3.jpg";
+
+
+const images = [imageOne, imageTwo, imageThree];
+
+const Carousel = () => {
+  const [curr, setCurr] = useState(0);
+  // current === images.length - 1 ? 0 : current + 1;
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurr((current) => {
+        if (current === images.length - 1) {
+          return 0;
+        } else {
+          return current + 1;
+        }
+      });
+    }, 5000);
+  }, []);
+
+  return (
+    <div >
+      {images.map(
+        (image, index) => index === curr && <img src={image} alt="test" />
+      )}
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ client/src/components/Organisations.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Organisations = () => {
+  return (
+    <div>
+      <h1 className="text-center"> Organisations Page </h1>
+    </div>
+  );
+};
+export default Organisations;
