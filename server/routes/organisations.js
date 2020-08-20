5398d528d6db599b295943d6800a79e6b6f3f056
ferhatu <ferhat_2001@yahoo.com>
2020-08-20 23:33:22 +0100

    route-organization

---
+++ server/routes/organisations.js
@@ -0,0 +1,109 @@
+const express = require ("express");
+const router = express.Router();
+const Org = require ("../models/organisation")
+router.get("/", (req,res)=>{
+  res.json({msg:"hello"})
+})
+router.get("/org", (req, res) => {
+  Org.find().then((org)=>{console.log(org); res.json(org)})
+});
+
+router.post("/org", (req, res) => {
+  const organisation = new Org({
+    logo: req.body.logo,
+    name: req.body.name,
+    category: req.body.category,
+    website: req.body.website,
+    location: req.body.location,
+    description: req.body.description
+  });
+
+  organisation
+    .save()
+    .then((org) => {
+      res.status(201).json(org);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});   
+
+router.get("/org/:organisationId", (req, res) => {
+  Org.findById(req.params.organisationId)
+    .exec()
+    .then((organisation) => {
+      console.log("from database", organisation);
+      if (organisation) {
+        res.status(200).json(organisation);
+      } else {
+        res.status(404).json({ message: "not valid entry" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.put("/org/:organisationId", (req, res) => {
+  const searchId = { _id: req.params.organisationId };
+  delete req.body.id;
+  const data = {};
+  if (req.body.name) {
+    data.name = req.body.name;
+  }
+  if (req.body.logo) {
+    data.logo = req.body.logo;
+  }
+  if (req.body.category) {
+    data.category = req.body.category;
+  }
+  if (req.body.website) {
+    data.website = req.body.website;
+  }
+  if (req.body.location) {
+    data.location = req.body.location;
+  }
+  if (req.body.description) {
+    data.description = req.body.description;
+  }
+  const options = { returnOriginal: false };
+
+  Org.findOneAndUpdate(searchId, { $set: data }, options)
+    .exec()
+    .then(() => {
+      res.status(200).json({ message: "Organisation Updated" });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.delete("/org/:organisationId", (req, res) => {
+  const id = req.params.organisationId;
+  Org.remove({ _id: id })
+    .exec()
+    .then(() => {
+      res.status(200).json({
+        message: "Organisation deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+
+module.exports = router;
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,60 +1,60 @@
-const express = require('express');
-const cors = require('cors');
-var path = require('path');
-const morgan = require('morgan')
-const mongoose=require('mongoose')
+const express = require("express");
+const cors = require("cors");
+var path = require("path");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const resources = require("./routes/resources");
+const organisations = require("./routes/organisations");
 const db = process.env.MONGO_URL;
 
 const app = express();
 app.use(cors());
-app.use(morgan('dev'))
-
-
+app.use(morgan("dev"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
-//connect to DB 
+app.use(bodyParser.json());
+//connect to DB
 mongoose
-  .connect(db, { useUnifiedTopology: true , useNewUrlParser: true })
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log("DB connected");
   })
   .catch((err) => console.log(err));
 
-
-app.get('/authors', (req, res) => {
-    console.log("mongo uri", process.env.MONGO_URL)
-    res.json({
-        
-        authors: ["Ferhat", "Minko", "Roxana", "Ahmad", "Irving"]
-    })
-})
+app.get("/authors", (req, res) => {
+  console.log("mongo uri", process.env.MONGO_URL);
+  res.json({
+    authors: ["Ferhat", "Minko", "Roxana", "Ahmad", "Irving"],
+  });
+});
 // Use Router for each api
 app.use("/api/resources", resources);
+// Router for Organisation
+app.use("/api/organisations", organisations);
 
 // if deployed, use react build as a code source.
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
-
 
 const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => {
-
-    process.env.PORT ?
-        console.log("app working on  << " + process.env.ROOT_URL + "  >> listening on port  " + Port) //deployed
-        :
-        console.log("app working on  http://localhost:" + Port);  //locally
-
-})
+  process.env.PORT
+    ? console.log(
+        "app working on  << " +
+          process.env.ROOT_URL +
+          "  >> listening on port  " +
+          Port
+      ) //deployed
+    : console.log("app working on  http://localhost:" + Port); //locally
+});
