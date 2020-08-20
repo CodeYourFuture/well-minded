f27c98411955e8eb33231767d5d9ead8e1bba86c
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-08-20 11:39:01 +0100

    create resource get ,post,put,delete route /install mongoose , morgan , dotenv / create model for resources

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 var path = require('path');
-const resources = require("./routes/resources");
-
+const morgan = require('morgan')
+const mongoose=require('mongoose')
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(morgan('dev'))
 
-// Use Router for each api
-app.use("/api/resources", resources);
+const resources = require("./routes/resources");
+const db = process.env.MONGO_URL
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+//connect to DB 
+mongoose
+  .connect(db, { useUnifiedTopology: true , useNewUrlParser: true })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => console.log(err));
+
 
 app.get('/authors', (req, res) => {
+    console.log("mongo uri", process.env.MONGO_URL)
     res.json({
+        
         authors: ["Ferhat", "Minko", "Roxana", "Ahmad", "Irving"]
     })
 })
@@ -24,11 +43,10 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-
+// Use Router for each api
+app.use("/api/resources", resources);
 
 const Port = process.env.PORT || 5000;
-
 
 app.listen(Port, () => {
 
