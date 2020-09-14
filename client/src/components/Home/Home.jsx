a53a4efb8986594501524dad3ac90411e315bc70
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-09-14 22:56:06 +0100

    update home page css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,35 +1,46 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Cards from "./Cards";
-import Entry from "./Entry";
-import OrganisationHelpButton from "./OrganisationHelpButton";
-import ViewResourcesButton from "./ViewResourceButton";
-import "../../css/cards.css";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import "../../css/home.css";
 
 const Home = () => {
   return (
     <>
       <Container className="home-page">
         <Row>
-          <Col>
-            <Entry />
+          <Col className="text-center">
+            <h1>Well-minded</h1>
           </Col>
         </Row>
         <Row>
-          <Col className="text-right">
-            <OrganisationHelpButton />
-          </Col>
-          <Col className="text-left">
-            <ViewResourcesButton />
+          <Col className="text-center">
+            <p>
+              Bring people and organisations together for free support and
+              mental health resources
+            </p>
           </Col>
         </Row>
-        <Row >
-        <Col>
-          <Cards />
-        </Col>
-      </Row>
+        <Row className="d-flex justify-content-between">
+          <Col className="text-right mb-3 mt-3">
+            <Link className="org-btn" to="/organisations">
+              Find Organisation to help
+            </Link>
+          </Col>
+          <Col className="text-left mb-3 mt-3">
+            <Link className="org-btn" to="/resources">
+              View More Resources
+            </Link>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Cards />
+          </Col>
+        </Row>
       </Container>
-      
     </>
   );
 };
