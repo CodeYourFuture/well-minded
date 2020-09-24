a11b11ad49486f9f02a9aed7c108956bd0795c55
Ahmad Ali <aallii300300@gmail.com>
2020-09-24 15:55:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,9 +47,9 @@ app.use("/api/contact", contact)
 
 // if deployed, use react build as a code source.
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build"));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
